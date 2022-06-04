bcd94000dc9ace5237e251a5384d323977c49b91
anhlt <tuananh.kirimaru@gmail.com>
2022-06-04 16:24:42 +0900

    update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,7 +29,7 @@ DIRECT_TEMPLATES = ['index', 'tags', 'categories',
 SITEMAP_SAVE_AS = 'sitemap.xml'
 
 # Theme
-THEME = 'themes/basic'
+THEME = 'themes/pelican-alchemy/alchemy'
 SITESUBTITLE = '\u2728 Suy nghĩ vu vơ'
 
 
